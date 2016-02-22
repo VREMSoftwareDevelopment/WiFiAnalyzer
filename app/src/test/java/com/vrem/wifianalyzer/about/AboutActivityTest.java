08985bcb4fcb14110f34e90158fe067bc73990f7
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2016-02-22 09:34:54 -0500

    updated copyright on all files

--- app/src/test/java/com/vrem/wifianalyzer/about/AboutActivityTest.java
+++ app/src/test/java/com/vrem/wifianalyzer/about/AboutActivityTest.java
@@ -13,6 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+
 package com.vrem.wifianalyzer.about;
 
 import android.support.v7.app.ActionBar;
--- app/src/test/java/com/vrem/wifianalyzer/wifi/model/StrengthTest.java
+++ app/src/test/java/com/vrem/wifianalyzer/wifi/model/StrengthTest.java
@@ -13,6 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
+
 package com.vrem.wifianalyzer.wifi.model;
 
 import android.net.wifi.WifiManager;
