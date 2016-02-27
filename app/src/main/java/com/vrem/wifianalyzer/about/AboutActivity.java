47476b02e8f6f6d9fbcab942decc67aa7ef61f32
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2016-02-27 16:05:10 -0500

    added automatic version number generation during the build cycle

--- app/src/main/java/com/vrem/wifianalyzer/about/AboutActivity.java
+++ app/src/main/java/com/vrem/wifianalyzer/about/AboutActivity.java
@@ -16,12 +16,15 @@
 
 package com.vrem.wifianalyzer.about;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.vrem.wifianalyzer.MainContext;
 import com.vrem.wifianalyzer.R;
@@ -40,10 +43,21 @@ public class AboutActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        setVersionNumber();
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void setVersionNumber() {
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            ((TextView) findViewById(R.id.version_info)).setText(packageInfo.versionName + " - " + packageInfo.versionCode);
+        } catch (PackageManager.NameNotFoundException e) {
+            mainContext.getLogger().error(this, "Version Information", e);
         }
     }
 
