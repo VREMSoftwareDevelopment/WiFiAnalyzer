745371232990ebf1f603175ce99d55a5a5265ad0
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2016-04-12 18:22:46 -0400

    added channel graph tests

---
+++ app/src/test/java/com/vrem/wifianalyzer/wifi/graph/channel/ChannelGraphFragmentTest.java
@@ -0,0 +1,63 @@
+/*
+ *    Copyright (C) 2015 - 2016 VREM Software Development <VREMSoftwareDevelopment@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.vrem.wifianalyzer.wifi.graph.channel;
+
+import com.vrem.wifianalyzer.BuildConfig;
+import com.vrem.wifianalyzer.MainContext;
+import com.vrem.wifianalyzer.RobolectricUtil;
+import com.vrem.wifianalyzer.wifi.scanner.Scanner;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.support.v4.SupportFragmentTestUtil;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class ChannelGraphFragmentTest {
+
+    private Scanner scanner;
+    private ChannelGraphFragment fixture;
+
+    @Before
+    public void setUp() throws Exception {
+        RobolectricUtil.INSTANCE.getMainActivity();
+
+        scanner = mock(Scanner.class);
+        MainContext.INSTANCE.setScanner(scanner);
+
+        fixture = new ChannelGraphFragment();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        RobolectricUtil.INSTANCE.restoreMainContext();
+    }
+
+    @Test
+    public void testOnCreateView() throws Exception {
+        SupportFragmentTestUtil.startFragment(fixture);
+        assertNotNull(fixture);
+    }
+
+}
