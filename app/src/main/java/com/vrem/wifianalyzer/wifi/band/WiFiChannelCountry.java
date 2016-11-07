9ca123b6345055c3e6a89f3cf50e0888ab7ff9e6
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2016-11-07 17:47:06 -0500

    adjust taiwan 2.4GHz channels

--- app/src/main/java/com/vrem/wifianalyzer/wifi/band/WiFiChannelCountry.java
+++ app/src/main/java/com/vrem/wifianalyzer/wifi/band/WiFiChannelCountry.java
@@ -569,7 +569,7 @@ public class WiFiChannelCountry {
             Arrays.asList(36, 40, 44, 48, 52, 56, 60, 64, 100, 104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 149, 153, 157, 161, 165)
         ),
         new WiFiChannelCountry("TW",
-            Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13),
+            Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),
             Arrays.asList(52, 56, 60, 64, 149, 153, 157, 161, 165)
         ),
         new WiFiChannelCountry("TZ",
