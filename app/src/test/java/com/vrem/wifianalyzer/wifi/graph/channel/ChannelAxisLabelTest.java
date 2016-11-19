f1a7ada09fd131e04f5f3e83a7060890254424cc
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2016-11-19 17:56:13 -0500

    change graph maximum Y scale to -20dBm

--- app/src/test/java/com/vrem/wifianalyzer/wifi/graph/channel/ChannelAxisLabelTest.java
+++ app/src/test/java/com/vrem/wifianalyzer/wifi/graph/channel/ChannelAxisLabelTest.java
@@ -61,7 +61,7 @@ public class ChannelAxisLabelTest {
     public void testYAxis() throws Exception {
         assertEquals(StringUtils.EMPTY, fixture.formatLabel(GraphViewBuilder.MIN_Y, false));
         assertEquals("-99", fixture.formatLabel(GraphViewBuilder.MIN_Y + 1, false));
-        assertEquals("-10", fixture.formatLabel(GraphViewBuilder.MAX_Y, false));
+        assertEquals("-20", fixture.formatLabel(GraphViewBuilder.MAX_Y, false));
         assertEquals(StringUtils.EMPTY, fixture.formatLabel(GraphViewBuilder.MAX_Y + 1, false));
     }
 
