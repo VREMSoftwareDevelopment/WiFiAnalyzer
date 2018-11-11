3087d2e76a119eee1cc524a7f4c0e3258e9e3913
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2018-11-11 11:15:27 -0500

    lint cleanup

--- app/src/main/java/com/vrem/util/FileUtils.java
+++ app/src/main/java/com/vrem/util/FileUtils.java
@@ -33,9 +33,7 @@ public class FileUtils {
 
     @NonNull
     public static String readFile(@NonNull Resources resources, @RawRes int id) {
-        InputStream inputStream = null;
-        try {
-            inputStream = resources.openRawResource(id);
+        try (InputStream inputStream = resources.openRawResource(id)) {
             int size = inputStream.available();
             byte[] bytes = new byte[size];
             int count = inputStream.read(bytes);
@@ -46,14 +44,6 @@ public class FileUtils {
         } catch (Exception e) {
             // file is corrupted
             return StringUtils.EMPTY;
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (Exception e) {
-                    // do nothing
-                }
-            }
         }
     }
 }
