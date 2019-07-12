eb2fcd07858911802726ae6e3f2793acaf815b77
VREM Software Development <VREMSoftwareDevelopment@gmail.com>
2019-07-12 07:35:21 -0400

    remove swipe left/right navigation

--- app/src/test/java/com/vrem/wifianalyzer/navigation/availability/NavigationOptionFactoryTest.java
+++ app/src/test/java/com/vrem/wifianalyzer/navigation/availability/NavigationOptionFactoryTest.java
@@ -31,72 +31,64 @@ public class NavigationOptionFactoryTest {
     @Test
     public void testRating() {
         List<NavigationOption> options = NavigationOptionFactory.RATING;
-        assertEquals(5, options.size());
+        assertEquals(4, options.size());
 
         assertTrue(options.contains(NavigationOptionFactory.WIFI_SWITCH_ON));
         assertTrue(options.contains(NavigationOptionFactory.SCANNER_SWITCH_ON));
         assertTrue(options.contains(NavigationOptionFactory.FILTER_OFF));
-        assertTrue(options.contains(NavigationOptionFactory.NEXT_PREV_ON));
         assertTrue(options.contains(NavigationOptionFactory.BOTTOM_NAV_ON));
 
         assertFalse(options.contains(NavigationOptionFactory.WIFI_SWITCH_OFF));
         assertFalse(options.contains(NavigationOptionFactory.SCANNER_SWITCH_OFF));
         assertFalse(options.contains(NavigationOptionFactory.FILTER_ON));
-        assertFalse(options.contains(NavigationOptionFactory.NEXT_PREV_OFF));
         assertFalse(options.contains(NavigationOptionFactory.BOTTOM_NAV_OFF));
     }
 
     @Test
     public void testOther() {
         List<NavigationOption> options = NavigationOptionFactory.OTHER;
-        assertEquals(5, options.size());
+        assertEquals(4, options.size());
 
         assertTrue(options.contains(NavigationOptionFactory.WIFI_SWITCH_ON));
         assertTrue(options.contains(NavigationOptionFactory.SCANNER_SWITCH_ON));
         assertTrue(options.contains(NavigationOptionFactory.FILTER_ON));
-        assertTrue(options.contains(NavigationOptionFactory.NEXT_PREV_ON));
         assertTrue(options.contains(NavigationOptionFactory.BOTTOM_NAV_ON));
 
         assertFalse(options.contains(NavigationOptionFactory.WIFI_SWITCH_OFF));
         assertFalse(options.contains(NavigationOptionFactory.SCANNER_SWITCH_OFF));
         assertFalse(options.contains(NavigationOptionFactory.FILTER_OFF));
-        assertFalse(options.contains(NavigationOptionFactory.NEXT_PREV_OFF));
         assertFalse(options.contains(NavigationOptionFactory.BOTTOM_NAV_OFF));
     }
 
     @Test
     public void testOff() {
         List<NavigationOption> options = NavigationOptionFactory.OFF;
-        assertEquals(5, options.size());
+        assertEquals(4, options.size());
 
         assertTrue(options.contains(NavigationOptionFactory.WIFI_SWITCH_OFF));
         assertTrue(options.contains(NavigationOptionFactory.SCANNER_SWITCH_OFF));
         assertTrue(options.contains(NavigationOptionFactory.FILTER_OFF));
-        assertTrue(options.contains(NavigationOptionFactory.NEXT_PREV_OFF));
         assertTrue(options.contains(NavigationOptionFactory.BOTTOM_NAV_OFF));
 
         assertFalse(options.contains(NavigationOptionFactory.WIFI_SWITCH_ON));
         assertFalse(options.contains(NavigationOptionFactory.SCANNER_SWITCH_ON));
         assertFalse(options.contains(NavigationOptionFactory.FILTER_ON));
-        assertFalse(options.contains(NavigationOptionFactory.NEXT_PREV_ON));
         assertFalse(options.contains(NavigationOptionFactory.BOTTOM_NAV_ON));
     }
 
     @Test
     public void testAccessPoints() {
         List<NavigationOption> options = NavigationOptionFactory.AP;
-        assertEquals(5, options.size());
+        assertEquals(4, options.size());
 
         assertTrue(options.contains(NavigationOptionFactory.WIFI_SWITCH_OFF));
         assertTrue(options.contains(NavigationOptionFactory.SCANNER_SWITCH_ON));
         assertTrue(options.contains(NavigationOptionFactory.FILTER_ON));
-        assertTrue(options.contains(NavigationOptionFactory.NEXT_PREV_ON));
         assertTrue(options.contains(NavigationOptionFactory.BOTTOM_NAV_ON));
 
         assertFalse(options.contains(NavigationOptionFactory.WIFI_SWITCH_ON));
         assertFalse(options.contains(NavigationOptionFactory.SCANNER_SWITCH_OFF));
         assertFalse(options.contains(NavigationOptionFactory.FILTER_OFF));
-        assertFalse(options.contains(NavigationOptionFactory.NEXT_PREV_OFF));
         assertFalse(options.contains(NavigationOptionFactory.BOTTOM_NAV_OFF));
     }
 
