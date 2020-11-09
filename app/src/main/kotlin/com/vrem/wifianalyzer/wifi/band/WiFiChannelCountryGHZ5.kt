3f2069ff0696c2212db1e90745d4703b127cdfdd
mrintrepide <40322493+mrintrepide@users.noreply.github.com>
2020-11-09 02:02:27 +0100

    Add ETSI region
    Add ETSI region without channel 144 and enable 169-173 (https://en.wikipedia.org/wiki/List_of_WLAN_channels)

--- app/src/main/kotlin/com/vrem/wifianalyzer/wifi/band/WiFiChannelCountryGHZ5.kt
+++ app/src/main/kotlin/com/vrem/wifianalyzer/wifi/band/WiFiChannelCountryGHZ5.kt
@@ -25,7 +25,10 @@ internal class WiFiChannelCountryGHZ5 {
     private val channelsSet3: SortedSet<Int> = sortedSetOf(149, 153, 157, 161, 165)
     private val channelsToExcludeCanada: SortedSet<Int> = sortedSetOf(120, 124, 128)
     private val channelsToExcludeIsrael: SortedSet<Int> = channelsSet2.union(channelsSet3).toSortedSet()
+    private val channelsToExcludeETSI: SortedSet<Int> = sortedSetOf(144)
+    private val channelsToIncludeETSI: SortedSet<Int> = sortedSetOf(169, 173)
     private val channels: SortedSet<Int> = channelsSet1.union(channelsSet2).union(channelsSet3).toSortedSet()
+
     private val channelsToExclude: Map<String, SortedSet<Int>> = mapOf(
             "AU" to channelsToExcludeCanada,    // Australia
             "CA" to channelsToExcludeCanada,    // Canada
@@ -34,11 +37,73 @@ internal class WiFiChannelCountryGHZ5 {
             "JP" to channelsSet3,               // Japan
             "KR" to channelsSet2,               // South Korea
             "TR" to channelsSet3,               // Turkey
-            "ZA" to channelsSet3                // South Africa
+            "ZA" to channelsSet3,               // South Africa
+            "AT" to channelsToExcludeETSI,      // ETSI Austria
+            "BE" to channelsToExcludeETSI,      // ETSI Belgium
+            "CH" to channelsToExcludeETSI,      // ETSI Switzerland
+            "CY" to channelsToExcludeETSI,      // ETSI Cyprus
+            "CZ" to channelsToExcludeETSI,      // ETSI Czechia
+            "DE" to channelsToExcludeETSI,      // ETSI Germany
+            "DK" to channelsToExcludeETSI,      // ETSI Denmark
+            "EE" to channelsToExcludeETSI,      // ETSI Estonia
+            "ES" to channelsToExcludeETSI,      // ETSI Spain
+            "FI" to channelsToExcludeETSI,      // ETSI Finland
+            "FR" to channelsToExcludeETSI,      // ETSI France
+            "GR" to channelsToExcludeETSI,      // ETSI Greece
+            "HU" to channelsToExcludeETSI,      // ETSI Hungary
+            "IE" to channelsToExcludeETSI,      // ETSI Ireland
+            "IS" to channelsToExcludeETSI,      // ETSI Iceland
+            "IT" to channelsToExcludeETSI,      // ETSI Italy
+            "LI" to channelsToExcludeETSI,      // ETSI Liechtenstein
+            "LT" to channelsToExcludeETSI,      // ETSI Lithuania
+            "LU" to channelsToExcludeETSI,      // ETSI Luxembourg
+            "LV" to channelsToExcludeETSI,      // ETSI Latvia
+            "MT" to channelsToExcludeETSI,      // ETSI Malta
+            "NL" to channelsToExcludeETSI,      // ETSI Netherlands
+            "NO" to channelsToExcludeETSI,      // ETSI Norway
+            "PL" to channelsToExcludeETSI,      // ETSI Poland
+            "PT" to channelsToExcludeETSI,      // ETSI Portugal
+            "RO" to channelsToExcludeETSI,      // ETSI Romania
+            "SE" to channelsToExcludeETSI,      // ETSI Sweden
+            "SI" to channelsToExcludeETSI,      // ETSI Slovenia
+            "SK" to channelsToExcludeETSI       // ETSI Slovakia
+    )
+
+    private val channelsToInclude: Map<String, SortedSet<Int>> = mapOf(
+            "AT" to channelsToIncludeETSI,      // ETSI Austria
+            "BE" to channelsToIncludeETSI,      // ETSI Belgium
+            "CH" to channelsToIncludeETSI,      // ETSI Switzerland
+            "CY" to channelsToIncludeETSI,      // ETSI Cyprus
+            "CZ" to channelsToIncludeETSI,      // ETSI Czechia
+            "DE" to channelsToIncludeETSI,      // ETSI Germany
+            "DK" to channelsToIncludeETSI,      // ETSI Denmark
+            "EE" to channelsToIncludeETSI,      // ETSI Estonia
+            "ES" to channelsToIncludeETSI,      // ETSI Spain
+            "FI" to channelsToIncludeETSI,      // ETSI Finland
+            "FR" to channelsToIncludeETSI,      // ETSI France
+            "GR" to channelsToIncludeETSI,      // ETSI Greece
+            "HU" to channelsToIncludeETSI,      // ETSI Hungary
+            "IE" to channelsToIncludeETSI,      // ETSI Ireland
+            "IS" to channelsToIncludeETSI,      // ETSI Iceland
+            "IT" to channelsToIncludeETSI,      // ETSI Italy
+            "LI" to channelsToIncludeETSI,      // ETSI Liechtenstein
+            "LT" to channelsToIncludeETSI,      // ETSI Lithuania
+            "LU" to channelsToIncludeETSI,      // ETSI Luxembourg
+            "LV" to channelsToIncludeETSI,      // ETSI Latvia
+            "MT" to channelsToIncludeETSI,      // ETSI Malta
+            "NL" to channelsToIncludeETSI,      // ETSI Netherlands
+            "NO" to channelsToIncludeETSI,      // ETSI Norway
+            "PL" to channelsToIncludeETSI,      // ETSI Poland
+            "PT" to channelsToIncludeETSI,      // ETSI Portugal
+            "RO" to channelsToIncludeETSI,      // ETSI Romania
+            "SE" to channelsToIncludeETSI,      // ETSI Sweden
+            "SI" to channelsToIncludeETSI,      // ETSI Slovenia
+            "SK" to channelsToIncludeETSI       // ETSI Slovakia
     )
 
     fun findChannels(countryCode: String): SortedSet<Int> =
             channels.subtract(channelsToExclude[countryCode.capitalize()]?.toSortedSet()
+                    ?: setOf()).union(channelsToInclude[countryCode.capitalize()]?.toSortedSet()
                     ?: setOf())
                     .toSortedSet()
 
