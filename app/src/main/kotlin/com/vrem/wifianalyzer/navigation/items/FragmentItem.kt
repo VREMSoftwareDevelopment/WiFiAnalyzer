7efa53b39fc3297dc889a1f1ec7df37ee5806fbf
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-03-31 22:06:11 -0400

    Utilize fragment commit extension function

--- app/src/main/kotlin/com/vrem/wifianalyzer/navigation/items/FragmentItem.kt
+++ app/src/main/kotlin/com/vrem/wifianalyzer/navigation/items/FragmentItem.kt
@@ -21,7 +21,7 @@ import android.view.MenuItem
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
 import com.vrem.wifianalyzer.MainActivity
 import com.vrem.wifianalyzer.R
 import com.vrem.wifianalyzer.navigation.NavigationMenu
@@ -36,8 +36,9 @@ internal class FragmentItem(val fragment: Fragment, override val registered: Boo
     }
 
     private fun startFragment(fragmentManager: FragmentManager) {
-        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.main_fragment, fragment).commit()
+        fragmentManager.commit {
+            replace(R.id.main_fragment, fragment)
+        }
     }
 
     private fun updateMainActivity(mainActivity: MainActivity, menuItem: MenuItem, navigationMenu: NavigationMenu) {
