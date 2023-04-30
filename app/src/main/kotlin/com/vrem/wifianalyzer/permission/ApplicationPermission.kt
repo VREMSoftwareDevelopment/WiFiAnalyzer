ef9adda96af097b9640452b35176a50e6f108b80
VREM Software Development <vremsoftwaredevelopment@gmail.com>
2023-04-30 10:50:38 -0400

    update dependencies

--- app/src/main/kotlin/com/vrem/wifianalyzer/permission/ApplicationPermission.kt
+++ app/src/main/kotlin/com/vrem/wifianalyzer/permission/ApplicationPermission.kt
@@ -28,20 +28,21 @@ class ApplicationPermission(
     private val permissionDialog: PermissionDialog = PermissionDialog(activity)
 ) {
     fun check() {
-        if (granted()) {
-            return
+        PERMISSIONS.forEach {
+            if (!granted(it) && !activity.isFinishing) {
+                permissionDialog.show()
+            }
         }
-        if (activity.isFinishing) {
-            return
-        }
-        permissionDialog.show()
     }
 
     fun granted(requestCode: Int, grantResults: IntArray): Boolean =
         requestCode == REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
 
     fun granted(): Boolean =
-        activity.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        PERMISSIONS.all { return granted(it) }
+
+    private fun granted(permission: String): Boolean =
+        activity.checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED
 
     companion object {
         internal val PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
